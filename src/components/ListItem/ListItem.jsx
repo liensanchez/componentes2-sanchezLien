fa4e40c3fd0173b47c38b630d7078d2c8c2228d1
Lien Sanchez <liensilviosanchez@gmail.com>
2022-03-22 14:58:01 -0300

    master

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import {useState} from 'react'
 
 
 
 function ListItem({greeting}) {
+  const [productos, setProductos]=useState(0)
 
- let productos= 0
+  //let productos=0
 
- const sumaProductos =()=>{
+  const sumaProductos =()=>{
 
-    productos ++
+    //productos ++
 
-    console.log(productos)
- }
+    //console.log(productos)
+
+
+    if (productos==10){
+
+      setProductos(productos+0)
+
+    }else{
+
+      setProductos(productos+1)
+      
+    }
+
+  } 
+
+  const restaProductos=()=>{
+
+    setProductos(productos-1)
+
+  }
 
   return (
 
     <>
 
     <h2>{greeting} </h2>
+
+    <button onClick={restaProductos}>-1</button>
 
     <label>{productos} </label>
 
